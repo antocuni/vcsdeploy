41bd93498e63476d61fca8832d076a0641534e08
antocuni <antocuni@localhost>
2010-08-08 21:32:47 +0200

    move the instantiation of MercurialRepo inside MercurialLogic

--- test/test_hg.py
+++ test/test_hg.py
@@ -1,8 +1,7 @@
 from vcsdeploy.logic import DefaultConfig
 from vcsdeploy.hg import MercurialLogic, MercurialRepo
 
-def pytest_funcarg__hg(request):
-    tmpdir = request.getfuncargvalue('tmpdir')
+def create_repo(tmpdir):
     hg = MercurialRepo(tmpdir, create=True)
     myfile = tmpdir.join('myfile.py')
 
@@ -20,9 +19,16 @@ def pytest_funcarg__hg(request):
     hg.tag('Version 1.1')
     return hg
 
+def pytest_funcarg__logic(request):
+    tmpdir = request.getfuncargvalue('tmpdir')
+    hg = create_repo(tmpdir)
+    config = DefaultConfig()
+    config.path = hg.path
+    return MercurialLogic(config)
+    
 
-def test_get_current_version(hg):
-    logic = MercurialLogic(hg, config=None)
+def test_get_current_version(logic):
+    hg = logic.hg
     assert logic.get_current_version() == 'Latest version'
     hg.update('Version 1.1')
     assert logic.get_current_version() == 'Version 1.1'
@@ -32,13 +38,12 @@ def test_get_current_version(hg):
     assert logic.get_current_version() == 'Latest version'
 
 
-def test_get_list_of_versions(hg):
-    logic = MercurialLogic(hg, config=DefaultConfig())
+def test_get_list_of_versions(logic):
     assert logic.get_list_of_versions() == ['Version 1.1', 'Version 1.0']
 
 
-def test_update_to(hg):
-    logic = MercurialLogic(hg, config=DefaultConfig())
+def test_update_to(logic):
+    hg = logic.hg
     myfile = hg.path.join('myfile.py')
     logic.update_to('Version 1.0')
     assert myfile.read() == 'version = 1.0'
