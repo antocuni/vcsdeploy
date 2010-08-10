d2db78fbf021b4fa76af264dbc87ffd0d2918c5c
antocuni <anto.cuni@gmail.com>
2010-08-10 16:25:02 +0200

    make sure to save generated files in the right location

--- vcsdeploy/makefile.py
+++ vcsdeploy/makefile.py
@@ -9,14 +9,14 @@ def check(in_, out):
 def make():
     for ui in DIR.visit('*.ui'):
         pyname = 'Ui_' + ui.basename.replace('.ui', '.py')
-        pyname = py.path.local(pyname)
+        pyname = ui.dirpath().join(pyname)
         if check(ui, pyname):
             print ui
             os.system('pyuic4 "%s" -o "%s"' % (ui, pyname))
 
     for rc in DIR.visit('*.qrc'):
         pyname = rc.basename.replace('.qrc', '_rc.py')
-        pyname = py.path.local(pyname)
+        pyname = rc.dirpath().join(pyname)
         if check(rc, pyname):
             print rc
             os.system('pyrcc4 "%s" -o "%s"' % (rc, pyname))
