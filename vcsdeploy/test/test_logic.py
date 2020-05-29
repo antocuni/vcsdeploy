a9a5cbde4f72974fce03f2a09d286889b40e0874
Antonio Cuni <anto.cuni@gmail.com>
2020-05-29 15:35:19 +0200

    migrate all hg tests into BaseLogicTest

--- vcsdeploy/test/test_logic.py
+++ vcsdeploy/test/test_logic.py
@@ -16,6 +16,85 @@ class BaseTestLogic(object):
         config.path = self.tmpdir
         return self.LogicClass(config)
 
+    def test_get_current_version(self, logic):
+        hg = logic.hg
+        assert logic.get_current_version() == 'Latest version'
+        hg.update('Version_1.1')
+        assert logic.get_current_version() == 'Version_1.1'
+        hg.update(rev='Version_1.0')
+        assert logic.get_current_version() == 'Version_1.0'
+        hg.update()
+        assert logic.get_current_version() == 'Latest version'
+        hg.update(rev=1)
+        assert logic.get_current_version() is None
+
+    def test_get_list_of_versions(self, logic):
+        assert logic.get_list_of_versions() == ['Version_1.1', 'Version_1.0']
+
+    def test_update_to(self, logic):
+        myfile = logic.config.path.join('myfile.py')
+        logic.update_to('Version_1.0')
+        assert myfile.read() == 'version = 1.0'
+        logic.update_to('Version_1.1')
+        assert myfile.read() == 'version = 1.1'
+
+    def test_unknown_revision(self, logic):
+        py.test.raises(UnknownRevisionError, "logic.update_to('foobar')")
+        py.test.raises(UnknownRevisionError, "logic.update_to('xxx 1.2.3')")
+
+    def test_get_current_revision(self, logic):
+        rev0 = logic.get_current_revision()
+        logic.update_to('Version_1.0')
+        rev1 = logic.get_current_revision()
+        assert rev0 != rev1
+        logic.update_to(rev0)
+        assert logic.get_current_revision() == rev0
+
+    def test_pull(self, tmpdir):
+        # setup a "remote" repo where the "development" will go on, and a "local"
+        # one where the program is installed. The idea is that the local will pull
+        # from the remote
+        remotedir = tmpdir.join('remote')
+        localdir = tmpdir.join('local')
+        self.create_test_repo(remotedir)
+        self.clone_test_repo(localdir)
+
+        # simulate some development
+        myfile_remote = remotedir.join('myfile.py')
+        myfile_remote.write('version = 1.2')
+        self.commit_file(myfile_remote, 'new fancy features', tag='Version_1.2')
+
+        # use Version_1.1 in production
+        config = DefaultConfig()
+        config.path = localdir
+        logic = self.LogicClass(config)
+        logic.update_to('Version_1.1')
+        assert logic.get_current_version() == 'Version_1.1'
+
+        # now, let's look for updates
+        logic.pull()
+        assert logic.get_current_version() == 'Version_1.1'
+        assert logic.get_list_of_versions() == ['Version_1.2', 'Version_1.1', 'Version_1.0']
+        logic.update_to('Version_1.2')
+        assert logic.get_current_version() == 'Version_1.2'
+
+    def test_log(self, logic):
+        logfile = self.tmpdir.join('log')
+        config = DefaultConfig()
+        config.path = logic.hg.path
+        config.logfile = str(logfile)
+        logic = MercurialLogic(config)
+        #
+        logic.update_to('Version_1.0')
+        rev0 = logic.get_current_revision()
+        logic.update_to('Version_1.1')
+        rev1 = logic.get_current_revision()
+        #
+        lines = logfile.readlines()
+        assert len(lines) == 2
+        assert lines[0].endswith('updated to: %s Version_1.0\n' % rev0)
+        assert lines[1].endswith('updated to: %s Version_1.1\n' % rev1)
+
 
 
 class TestHgLogic(BaseTestLogic):
@@ -38,85 +117,5 @@ class TestHgLogic(BaseTestLogic):
         if tag:
             self._hg.tag(tag)
 
-    def test_get_current_version(self, logic):
-        hg = logic.hg
-        assert logic.get_current_version() == 'Latest version'
-        hg.update('Version_1.1')
-        assert logic.get_current_version() == 'Version_1.1'
-        hg.update(rev='Version_1.0')
-        assert logic.get_current_version() == 'Version_1.0'
-        hg.update()
-        assert logic.get_current_version() == 'Latest version'
-        hg.update(rev=1)
-        assert logic.get_current_version() is None
-
-
-def test_get_list_of_versions(logic):
-    assert logic.get_list_of_versions() == ['Version 1.1', 'Version 1.0']
-
-
-def test_update_to(logic):
-    myfile = logic.config.path.join('myfile.py')
-    logic.update_to('Version 1.0')
-    assert myfile.read() == 'version = 1.0'
-    logic.update_to('Version 1.1')
-    assert myfile.read() == 'version = 1.1'
-
-def test_unknown_revision(logic):
-    py.test.raises(UnknownRevisionError, "logic.update_to('foobar')")
-    py.test.raises(UnknownRevisionError, "logic.update_to('xxx 1.2.3')")
-
-def test_get_current_revision(logic):
-    rev0 = logic.get_current_revision()
-    logic.update_to('Version 1.0')
-    rev1 = logic.get_current_revision()
-    assert rev0 != rev1
-    logic.update_to(rev0)
-    assert logic.get_current_revision() == rev0
-
-
-def test_pull(tmpdir):
-    # setup a "remote" repo where the "development" will go on, and a "local"
-    # one where the program is installed. The idea is that the local will pull
-    # from the remote
-    remotedir = tmpdir.join('remote')
-    localdir = tmpdir.join('local')
-    hg_remote = create_repo(remotedir)
-    hg_remote.clone(localdir)
-
-    # simulate some development
-    myfile_remote = remotedir.join('myfile.py')
-    myfile_remote.write('version = 1.2')
-    hg_remote.commit(message='new fancy features')
-    hg_remote.tag('Version 1.2')
-
-    # use Version 1.1 in production
-    config = DefaultConfig()
-    config.path = localdir
-    logic = MercurialLogic(config)
-    logic.update_to('Version 1.1')
-    assert logic.get_current_version() == 'Version 1.1'
-
-    # now, let's look for updates
-    logic.pull()
-    assert logic.get_current_version() == 'Version 1.1'
-    assert logic.get_list_of_versions() == ['Version 1.2', 'Version 1.1', 'Version 1.0']
-    logic.update_to('Version 1.2')
-    assert logic.get_current_version() == 'Version 1.2'
-
-def test_log(tmpdir, logic):
-    logfile = tmpdir.join('log')
-    config = DefaultConfig()
-    config.path = logic.hg.path
-    config.logfile = str(logfile)
-    logic = MercurialLogic(config)
-    #
-    logic.update_to('Version 1.0')
-    rev0 = logic.get_current_revision()
-    logic.update_to('Version 1.1')
-    rev1 = logic.get_current_revision()
-    #
-    lines = logfile.readlines()
-    assert len(lines) == 2
-    assert lines[0].endswith('updated to: %s Version 1.0\n' % rev0)
-    assert lines[1].endswith('updated to: %s Version 1.1\n' % rev1)
+    def clone_test_repo(self, dst):
+        self._hg.clone(dst)
