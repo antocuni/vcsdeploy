bc9ee5ece9f5f66088114a9ef22ebd1c4077a1be
Antonio Cuni <anto.cuni@gmail.com>
2025-08-09 13:00:21 +0200

    make sure to sort 10.0 after 9.9

--- vcsdeploy/gitlogic.py
+++ vcsdeploy/gitlogic.py
@@ -5,6 +5,18 @@ import git
 from vcsdeploy.logic import AbstractLogic, UnknownRevisionError
 
 
+def _version_key(version):
+    parts = version.split(".")
+    key = []
+    for part in parts:
+        match = re.match(r'(\d+)', part)
+        if match:
+            key.append(int(match.group(1)))
+        else:
+            key.append(-1)  # non-numeric parts sort lower
+    return key
+
+
 class GitLogic(AbstractLogic):
 
     def __init__(self, config):
@@ -30,8 +42,7 @@ class GitLogic(AbstractLogic):
         for tag in self.repo.tags:
             if re.match(self.config.version_regex, tag.name):
                 versions.append(tag.name)
-        # repo.tags are from oldest to newest, but we want the reverse
-        versions.reverse()
+        versions.sort(key=_version_key, reverse=True)
         return versions
 
     def update_to(self, version):
